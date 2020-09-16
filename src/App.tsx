6ab4725eb07535d4ddb60748f30606bc4c9d4be0
pedroksty <pedrotrab2132@gmail.com>
2020-09-15 23:41:03 -0300

    Create FistAndSecond Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import './App.css'
 
-function App() {
+import GlobalStyle from './styles/global'
+
+import { Main, Section, Sticky } from './styles'
+
+import FirstAndSecond from './components/FirstAndSecond'
+
+const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <GlobalStyle />
+
+      <Main style={{ height: '1610vh' }}>
+        <h1>dale</h1>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
     </>
   )
 }
